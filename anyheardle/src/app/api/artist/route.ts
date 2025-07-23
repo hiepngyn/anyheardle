35afc25d9f59235a4cfee16468df631a31bba029
Hiep Nguyen <22hiepnguyen@gmail.com>
2025-07-22 21:30:27 -0500

    artist info api

--- anyheardle/src/app/api/artist/route.ts
+++ anyheardle/src/app/api/artist/route.ts
@@ -1,1 +1,32 @@
-//GET https://api.spotify.com/v1/search?q={artist_name}&type=artist&limit=1
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest){
+    const artistName = req.nextUrl.searchParams.get('name');
+    const artistLimit = req.nextUrl.searchParams.get('quantity');
+    if(!artistName) return NextResponse.json({error: "Missing artist name"})
+    if(!artistLimit) return NextResponse.json({error: "Missing artist limit"})
+
+
+    const tokenRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/spotify-token`);
+    const { access_token } = await tokenRes.json()
+
+    const searchRes = await fetch(
+        `https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist&limit=${encodeURIComponent(artistLimit)}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+    );
+    const data = await searchRes.json();
+
+    if (!data.artists.items.length) {
+    return NextResponse.json({ error: 'Artist not found' }, { status: 404 });
+    }
+
+    const artists = data.artists.items.map((artist: any) => ({
+      id: artist.id,
+      name: artist.name
+    }));
+    return NextResponse.json({ artists });
+}
